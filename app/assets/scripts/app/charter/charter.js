2169c1f2a03b95754685c38749568fdd6ef54b65
Mark Hanna <cipscis@gmail.com>
2019-06-27 19:55:49 +1200

    Readjusted tooltips for horizontal bar charts, and made some template improvements

--- app/assets/scripts/app/charter/charter.js
+++ app/assets/scripts/app/charter/charter.js
@@ -457,6 +457,7 @@ define(
 						dataPoint.displayValue = Charter._getDisplayNumber(dataPoint.value, axisConfig);
 						dataPoint.label = chartData.labels[j];
 						dataPoint.dataSeries = dataSeries.name;
+						dataPoint.hasDataSeries = !!dataPoint.dataSeries;
 					}
 				}
 
@@ -515,6 +516,7 @@ define(
 
 				var i, series,
 					j, dataPoint,
+					obj,
 					objArray;
 
 				for (i = 0; i < dataSeries.length; i++) {
@@ -523,18 +525,23 @@ define(
 
 					for (j = 0; j < series.dataPoints.length; j++) {
 						dataPoint = series.dataPoints[j];
+						obj = {};
 
 						if (dataPoint instanceof Object) {
-							objArray.push({
+							obj = {
 								value: dataPoint.value,
 								color: dataPoint.color || series.color
-							});
+							};
 						} else {
-							objArray.push({
+							obj = {
 								value: dataPoint,
 								color: series.color
-							});
+							};
 						}
+
+						obj.hasColor = !!obj.color;
+
+						objArray.push(obj);
 					}
 
 					dataSeries[i].dataPoints = objArray;
