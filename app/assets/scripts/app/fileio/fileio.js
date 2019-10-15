6d224848823957ccfbf63246b9815a3bba73d5f1
Brooke Hart <brooke+github@curly.kiwi>
2019-10-15 14:03:18 +1300

    Added Analyser.saveComparisonSummaryCsv

---
+++ app/assets/scripts/app/fileio/fileio.js
@@ -0,0 +1,52 @@
+define(
+	[],
+
+	function () {
+		const fileIO = (function () {
+			let $link;
+
+			const module = {
+				save: {
+					data: function (data, filename, type) {
+						// Construct a Blob and download it
+						let blob = new Blob(
+							[data],
+							{
+								type: type || 'text/plain'
+							}
+						);
+
+						module.save.blob(blob, filename);
+					},
+
+					blob: function (blob, filename) {
+						if (navigator.msSaveBlob) {
+							navigator.msSaveBlob(blob, filename);
+						} else {
+							let url = URL.createObjectURL(blob);
+							module.save._downloadDataUrl(url, filename);
+						}
+					},
+
+					_downloadDataUrl: function (dataUrl, filename) {
+						$link = $link || document.createElement('a');
+						$link.href = dataUrl;
+						$link.download = filename;
+						$link.click();
+
+						URL.revokeObjectURL(dataUrl);
+					},
+				}
+			};
+
+			return {
+				save: {
+					data: module.save.data,
+					blob: module.save.blob,
+				}
+			};
+		})();
+
+		return fileIO;
+	}
+);
